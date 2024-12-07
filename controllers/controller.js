5a0d7404223e2c5e1e7cc9b3082e284372c2aa80
MauAnalyst <164758301+MauAnalyst@users.noreply.github.com>
2024-12-07 08:05:19 -0300

    feat: routes pages navgations

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,3 +1,5 @@
+import { DataPages } from "./pages.js";
+
 const RespIA = (req, res) => {
   try {
   } catch (error) {
@@ -8,7 +10,7 @@ const RespIA = (req, res) => {
 
 const Chatpages = (req, res) => {
   try {
-    res.redirect("/chats/geral");
+    res.redirect("/chats/segregados");
   } catch (error) {
     console.log(error);
     res.status(500).send("Erro ao carregar a página.");
@@ -28,4 +30,19 @@ const AcessPages = (req, res) => {
   }
 };
 
-export { RespIA, Chatpages, AcessPages };
+const GetPageContent = (req, res) => {
+  const { processo } = req.params;
+  try {
+    res.json({
+      content: `
+    <span style="display: none">Geral</span>
+    <h1>${DataPages[processo].title}</h1>
+    <p>${DataPages[processo].text}<p>`,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erro ao carregar conteúdo dinâmico.");
+  }
+};
+
+export { RespIA, Chatpages, AcessPages, GetPageContent };
