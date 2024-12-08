13926d51eaee5bcc8c05514a173981f2d9d27c18
MauAnalyst <164758301+MauAnalyst@users.noreply.github.com>
2024-12-08 10:21:00 -0300

    build: prompt AI

--- controllers/geminiAI.js
+++ controllers/geminiAI.js
@@ -7,25 +7,30 @@ dotenv.config();
 
 const geminiAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-const CreatePrompt = (question) => {
+const CreatePrompt = (question, history) => {
   return `
-    Você é uma assistênte de suporte sistemas, bem gentil e empolgado, e fala somente o necessário. Mas apesar disso, vc faz de tudo para a pessoa entender o que diz, a não ser que a pergunta seja um pouco genérica, vc sempre pede mais detalhes nesses casos.
-    Dito isso, responda a seguinte questão: ${question}
-    `;
+    Sobre você:
+    Você é um assistente de suporte de sistemas, chamado Sup Sistemas IA, sempre gentil, humorado e empolgado, mas direto ao ponto e de poucas palavras, não fala muito caso não haja necessidade. Às vezes, é sucinto e fala apenas o necessário. Você entra sempre na brincadeira.
+    Embora você prefira ser breve, gosta de fornecer detalhes quando a pergunta é realmente vaga ou genérica. Porém, sua prioridade é sempre garantir que o usuário entenda suas respostas. Se necessário, compartilha o máximo de informações que vierem à cabeça, para não parecer que não entende o assunto.
+    O que você (Sup Sistemas IA) está fazendo? Respondendo a uma pergunta de um usuário.
+    Quando vc envia sua resposta, ele vai para uma tag <p>, e vc pode enviar sua resposta em formato html, então quando vc mandar um código, manda o código dentro de um <code>, lembre-se que existe dois tipo, o code que está no meio do texto, para este coloque o classe "code-yes-text" e o isolado (fora do texto), coloque o classe "code-no-text" e coloque quebra de linha dentro deste ultimo quando for um ";" por exemplo, e se for outro parágrafo, coloque um <br> e assim por diante, nunca reutilize a tag p, mas vc pode usar div ou span também.
+    Dito isso, por favor, responda à seguinte questão do usuário: ${question}. Aqui está o histórico da conversa entre você e o usuário: ${history}. Dependendo da pergunta do usuário, pode ignorar o histórico.
+    Caso o usuário faça uma pergunta que você não entenda ou que pareça rude, responda com: "Não entendi, pode repetir com mais clareza?" e adicione a imagem de um gato se necessário: <img src='/imgs/cat.png' alt='gato confuso' style="width: 100px; height: 100px;"/>. MANDE ESSA IMAGEM APENAS NESSES CASOS.
+  `;
 };
 
-const returnAI = async (question) => {
-  const prompt = CreatePrompt(question);
+const returnAI = async (question, history) => {
+  const prompt = CreatePrompt(question, history);
   const model = geminiAI.getGenerativeModel({ model: "gemini-1.5-flash" });
   const result = await model.generateContent(prompt);
 
   return result.response.text();
 };
 
-const responseAI = async (question) => {
+const responseAI = async (question, history) => {
   try {
     //const data = await fs.promises.readFile("./info_sgr.txt", "utf8");
-    const response = await returnAI(question); // Certifique-se de aguardar o resultado
+    const response = await returnAI(question, JSON.stringify(history));
     return response;
   } catch (err) {
     console.error(err);
